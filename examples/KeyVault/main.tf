2ea10c4c288e63f95099cf3c4cdf9fcbc50c2877
TheIronRock95 <58424695+TheIronRock95@users.noreply.github.com>
2023-06-29 11:56:54 +0200

    Revert "fix: module fixes"
    This reverts commit b6f98470856e9b412f121f0205c9658f6e8e7696.

--- examples/KeyVault/main.tf
+++ examples/KeyVault/main.tf
@@ -2,16 +2,15 @@ module "key_vault" {
   source  = "sironite/key_vault/azurerm"
   version = "X.X.X"
 
-  key_vault_name           = "key-vault"
+  name                     = "key-vault"
   resource_group_name      = "resource-group"
   location                 = "westeurope"
   sku_name                 = "standard"
   tenant_id                = data.azurerm_client_config.current.tenant_id
+  soft_delete_enabled      = true
+  purge_protection_enabled = true
+  purge_protection_days    = 90
 
-  purge_protection_enabled = var.purge_protection_enabled
-  soft_delete_retention_days = var.soft_delete_retention_days
-  public_network_access_enabled = var.public_network_access_enabled
-  
   access_policies = {
     "access-policy-1" = {
       tenant_id               = data.azurerm_client_config.current.tenant_id
